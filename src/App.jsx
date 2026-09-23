37efef0d4f160e7eb15a61627d8437524af62f15
ibrahim <ibrahimabiodun069@gmail.com>
2026-09-23 21:58:30 +0100

    scroll to the top fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
@@ -20,11 +21,23 @@ import About from "./pages/About";
 import FAQ from "./pages/FAQ";
 import Contact from "./pages/Contact";
 
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <CartProvider>
+          <ScrollToTop />
           <Navbar />
           <Routes>
             <Route path="/"                element={<Products />} />
